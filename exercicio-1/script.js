4870e583845ed49773b7b8823a0796feb0c90b63
Larissa <larissabmac@gmail.com>
2024-01-18 19:38:57 -0300

    Primeira prática

---
+++ exercicio-1/script.js
@@ -0,0 +1,25 @@
+let caixa = document.querySelector(".caixa")
+
+function vermelho() {
+    caixa.classList.remove('reset', 'caixa-azul', 'caixa-amarela')
+    caixa.classList.add('caixa-vermelha')
+}
+document.querySelector("#vermelho").addEventListener('click', vermelho)
+
+function azul() {
+    caixa.classList.remove('caixa-vermelha', 'reset', 'caixa-amarela')
+    caixa.classList.add('caixa-azul')
+}
+document.querySelector("#azul").addEventListener('click', azul)
+
+function amarelo() {
+    caixa.classList.remove('caixa-vermelha', 'caixa-azul', 'reset')
+    caixa.classList.add('caixa-amarela')
+}
+document.querySelector("#amarelo").addEventListener('click', amarelo)
+
+function reset() {
+    caixa.classList.remove('caixa-vermelha', 'caixa-azul', 'caixa-amarela')
+    caixa.classList.add('reset')
+}
+document.querySelector("#reset").addEventListener('click', reset)
